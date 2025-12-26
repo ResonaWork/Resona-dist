cadffac0f0cb6ee067aad54ed124f08a5642ae9e
Edmilson britto de Almeida <240203185+ResonaWork@users.noreply.github.com>
2025-12-26 14:05:41 -0300

    **Versão:** 3.30.5
    Otimizações de Performance - LCP Fase 4 - Caching Strategy

---
+++ assets/DownloadIcon-CExEpcWn.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-5Wja44cn.js";const s=({className:r="w-6 h-6"})=>o.jsx("svg",{xmlns:"http://www.w3.org/2000/svg",className:r,fill:"none",viewBox:"0 0 24 24",stroke:"currentColor",strokeWidth:2,children:o.jsx("path",{strokeLinecap:"round",strokeLinejoin:"round",d:"M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"})});export{s as D};
