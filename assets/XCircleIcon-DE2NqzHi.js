5f07cb6d74105c59db10337a5f42ee80c55b6b21
Edmilson britto de Almeida <240203185+ResonaWork@users.noreply.github.com>
2025-12-26 13:10:16 -0300

    Otimizações de Performance - LCP Fase 2

---
+++ assets/XCircleIcon-DE2NqzHi.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BuN687Wf.js";const t=({className:r="w-6 h-6"})=>o.jsx("svg",{xmlns:"http://www.w3.org/2000/svg",className:r,fill:"none",viewBox:"0 0 24 24",stroke:"currentColor",strokeWidth:2,children:o.jsx("path",{strokeLinecap:"round",strokeLinejoin:"round",d:"M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"})});export{t as X};
