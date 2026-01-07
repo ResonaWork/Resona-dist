cc13e15c8ee3619629d873551e09847d7197b236
Edmilson britto de Almeida <240203185+ResonaWork@users.noreply.github.com>
2026-01-07 13:39:10 -0300

    novo

---
+++ assets/ClockIcon-DIEPyEYm.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BRiOCH-Z.js";const t=({className:r="w-6 h-6"})=>o.jsx("svg",{xmlns:"http://www.w3.org/2000/svg",className:r,fill:"none",viewBox:"0 0 24 24",stroke:"currentColor",strokeWidth:2,children:o.jsx("path",{strokeLinecap:"round",strokeLinejoin:"round",d:"M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"})});export{t as C};
