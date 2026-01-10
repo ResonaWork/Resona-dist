909b8d9111f48e871be35cbd9c61712c2eb238ae
Edmilson britto de Almeida <240203185+ResonaWork@users.noreply.github.com>
2026-01-10 14:05:49 -0300

    Overhaul de Páginas Institucionais: Sobre e FAQ

--- assets/ArrowLeftIcon-eAdbvVt2.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-TxEahy0v.js";const s=({className:r="w-6 h-6"})=>o.jsx("svg",{xmlns:"http://www.w3.org/2000/svg",className:r,fill:"none",viewBox:"0 0 24 24",stroke:"currentColor",strokeWidth:2,children:o.jsx("path",{strokeLinecap:"round",strokeLinejoin:"round",d:"M10 19l-7-7m0 0l7-7m-7 7h18"})});export{s as A};
